61284b39b2e7023e1085d19302423d441cd975ce
Mwenda <kelvinfotech@gmail.com>
2024-06-02 17:16:11 +0300

    Python P3 V2 Mapping Python Class To Db

--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,46 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        CURSOR.execute("DROP TABLE IF EXISTS departments")
+
+    def save(self):
+        sql = """
+            INSERT INTO departments (name, location) VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.location))
+        self.id = CURSOR.lastrowid
+        CONN.commit()
+
+    @classmethod
+    def create(cls, name, location):
+        department = cls(name, location)
+        department.save()
+        return department
+
+    def update(self):
+        sql = """
+            UPDATE departments SET name=?, location=? WHERE id=?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        sql = """
+            DELETE FROM departments WHERE id=?
+        """
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
